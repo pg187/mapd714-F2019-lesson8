d6facc4cd83f3ae7ee8af17ebb3afcfd9e8061a9
Pragya <prog187.pg@gmail.com>
2019-10-30 18:49:22 -0400

    Initial Commit

---
+++ mapd714-F2019-lesson8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapd714-F2019-lesson8
+//
+//  Created by Student on 2019-10-30.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
